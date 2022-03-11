c77cce9b821b78a38360507ca43f17ca9b44b682
iimelapp <87647753+iimelapp@users.noreply.github.com>
2022-03-11 17:47:36 +0200

    tehtävät 3.1 - 3.22

--- index.js
+++ index.js
@@ -10,10 +10,25 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 app.use(express.static('build'))
 
-morgan.token('body', function (req, res) { 
+morgan.token('body', function (req) {
     return [
         JSON.stringify(req.body)
-    ] 
+    ]
+})
+
+app.post('/api/persons', (req, res, next) => {
+    const body = req.body
+
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    person.save()
+        .then(savedPerson => {
+            res.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons', (req, res) => {
@@ -22,39 +37,53 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person =>{
-        res.json(person)
-    })
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            res.json(person)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
-    res.send(`<p>Phone book has info for ${persons.length} people </p> <p>${new Date()}</p>`
-    )
+    Person.find({}).then(persons => {
+        res.send(`<p>Phonebook has info for ${persons.length} people
+        <p>${new Date()}</p>`)
+    })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+app.put('api/persons/:id', (req, res, next) => {
+    const { name, number } = req.body
+
+    Person.findByIdAndUpdate(req.params.id,
+        { name, number },
+        { new: true, runValidators: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
-    const body = req.body
-
-    if (body.name === undefined || body.number === undefined) {
-        return res.json.status(400).json({error: 'name or number missing'})
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
     }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
+    next(error)
+}
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
-})
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
